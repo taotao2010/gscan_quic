fc6fe40e7d20cd381b80b185ab6baa55f31f9881
taotao2010 <cih002@gmail.com>
2017-11-24 21:03:04 +0800

    up

--- vendor/github.com/phuslu/quic-go/internal/wire/ack_range.go
+++
@@ -1,9 +0,0 @@
-package wire
-
-import "github.com/phuslu/quic-go/internal/protocol"
-
-// AckRange is an ACK range
-type AckRange struct {
-	FirstPacketNumber protocol.PacketNumber
-	LastPacketNumber  protocol.PacketNumber
-}
